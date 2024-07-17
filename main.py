a8640b0ddcb8c60bf2809c554937d52a0f16d45c
DanielChahine0 <chahined@my.yorku.ca>
2024-07-17 16:19:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import customtkinter as ct
+from PIL import Image
 import tkinter as tk
 
 
 class PiggyBank:
     def __init__(self):
-        # Configuration Section
         self.WIDTH = 1200
         self.HEIGHT = 600
+        self.pigSize = 250
+        self.pigMargin = 20
+
+        # Configuration Section
         self.root = ct.CTk()
         self.root.iconbitmap("Images/piggy bank.ico")
         self.root.geometry(str(self.WIDTH) + "x" + str(self.HEIGHT)+"+100+100")
         self.root.title("FPB - Fat Piggy Bank")
+        ct.set_appearance_mode("Light")
+        self.root.resizable(False, False)
 
         # Menu Section
         self.menubar = tk.Menu(self.root)
@@ -21,18 +27,13 @@ class PiggyBank:
         self.menubar.add_cascade(menu=self.fileMenu, label="File")
         self.root.configure(menu=self.menubar)
 
-        # Frame Section
-        self.mainframe = ct.CTkFrame(self.root)
-        self.mainframe.columnconfigure(0, weight=1)
-        self.mainframe.columnconfigure(1, weight=1)
-        self.mainframe.columnconfigure(2, weight=1)
-        self.btn = ct.CTkButton(self.mainframe, text="1")
-        self.btn1 = ct.CTkButton(self.mainframe, text="2")
-        self.btn.grid(row=0, column=1, sticky=ct.W+ct.E)
-        self.btn1.grid(row=1, column=0, sticky=ct.W+ct.E)
+        # Piggy Bank Photo
+        self.img = Image.open("Images/piggy bank.png")
+        self.image = ct.CTkImage(light_image=self.img, size=(self.pigSize, self.pigSize))
+        self.panel = ct.CTkLabel(self.root, image=self.image, text='')
+        self.panel.place(x=self.WIDTH-self.pigSize-self.pigMargin*3, y=self.pigMargin)
 
-        self.mainframe.pack(fill='x')
-
+        # Habit Input Text
 
 
         self.root.mainloop()
